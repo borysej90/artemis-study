9b942af6f4849dc3200172033c9bb56bd62bb914
borysej90 <borysej90@gmail.com>
2022-06-02 18:05:04 -0400

    Moved to Maven

--- src/main/java/org/example/consumer/Consumer.java
+++ src/main/java/org/example/consumer/Consumer.java
@@ -1,26 +1,20 @@
-package producer;
+package org.example.consumer;
 
-import org.apache.activemq.artemis.api.core.ActiveMQBuffer;
 import org.apache.activemq.artemis.api.core.ActiveMQException;
 import org.apache.activemq.artemis.api.core.client.*;
 
-public class Producer {
+public class Consumer {
     public static void main(String[] args) throws Exception {
 
         ServerLocator locator = ActiveMQClient.createServerLocator("tcp://localhost:61616");
         ClientSession session = locator.createSessionFactory().createSession();
         gracefulShutdown(session);
 
-        ClientProducer producer = session.createProducer("anyq.addr");
+        ClientConsumer consumer = session.createConsumer("anyq");
         session.start();
-        int counter = 0;
         while (true) {
-            ClientMessage msg = session.createMessage(true);
-            ActiveMQBuffer buffer = msg.getBodyBuffer();
-            buffer.writeString("The Message - " + counter);
-            producer.send(msg);
-            System.out.println("Sent \"" + buffer.readString() + '"');
-            counter++;
+            ClientMessage msg = consumer.receive();
+            System.out.println("Received \"" + msg.getBodyBuffer().readString() + '"');
         }
     }
 
--- src/main/java/org/example/producer/Producer.java
+++ src/main/java/org/example/producer/Producer.java
@@ -1,20 +1,26 @@
-package consumer;
+package org.example.producer;
 
+import org.apache.activemq.artemis.api.core.ActiveMQBuffer;
 import org.apache.activemq.artemis.api.core.ActiveMQException;
 import org.apache.activemq.artemis.api.core.client.*;
 
-public class Consumer {
+public class Producer {
     public static void main(String[] args) throws Exception {
 
         ServerLocator locator = ActiveMQClient.createServerLocator("tcp://localhost:61616");
         ClientSession session = locator.createSessionFactory().createSession();
         gracefulShutdown(session);
 
-        ClientConsumer consumer = session.createConsumer("anyq");
+        ClientProducer producer = session.createProducer("anyq.addr");
         session.start();
+        int counter = 0;
         while (true) {
-            ClientMessage msg = consumer.receive();
-            System.out.println("Received \"" + msg.getBodyBuffer().readString() + '"');
+            ClientMessage msg = session.createMessage(true);
+            ActiveMQBuffer buffer = msg.getBodyBuffer();
+            buffer.writeString("The Message - " + counter);
+            producer.send(msg);
+            System.out.println("Sent \"" + buffer.readString() + '"');
+            counter++;
         }
     }
 
